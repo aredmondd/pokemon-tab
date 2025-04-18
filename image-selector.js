69e90c1acacc2d50bfe2efd9cffd16ac86725359
Aiden Redmond <adnrdmnd@gmail.com>
2025-04-18 11:19:17 -0400

    display shinies

--- image-selector.js
+++ image-selector.js
@@ -1,18 +1,38 @@
 let images = ['ditto-shiny.gif', 'pikachu-cosplay-shiny.gif', 'abra-shiny.gif', 'paras.gif', 'hitmonlee.gif', 'hitmonlee-shiny.gif', 'seadra.gif', 'poliwrath-shiny.gif', 'poliwhirl.gif', 'seel-shiny.gif', 'moltres-shiny.gif', 'arcanine.gif', 'aerodactyl.gif', 'mewtwo-shiny.gif', 'staryu.gif', 'muk.gif', 'fearow.gif', 'kadabra-shiny.gif', 'jolteon-shiny.gif', 'dugtrio-shiny.gif', 'spearow.gif', 'graveler-shiny.gif', 'farfetchd.gif', 'diglett.gif', 'raichu-shiny.gif', 'drowzee-shiny.gif', 'slowbro.gif', 'nidoran_f-shiny.gif', 'staryu-shiny.gif', 'vaporeon.gif', 'kabuto-shiny.gif', 'dragonair-shiny.gif', 'wigglytuff-shiny.gif', 'machoke.gif', 'dratini-shiny.gif', 'poliwag.gif', 'dugtrio.gif', 'hitmonchan-shiny.gif', 'kingler-shiny.gif', 'kabutops.gif', 'magnemite-shiny.gif', 'oddish.gif', 'pikachu-cosplay.gif', 'chansey-shiny.gif', 'pidgeot.gif', 'magmar-shiny.gif', 'sandslash.gif', 'chansey.gif', 'ponyta-shiny.gif', 'kangaskhan.gif', 'golduck.gif', 'haunter.gif', 'poliwhirl-shiny.gif', '.DS_Store', 'zapdos.gif', 'clefable-shiny.gif', 'butterfree-shiny.gif', 'ponyta.gif', 'raichu.gif', 'porygon-shiny.gif', 'hitmonchan.gif', 'caterpie.gif', 'psyduck.gif', 'bellsprout-shiny.gif', 'arcanine-shiny.gif', 'venomoth-shiny.gif', 'nidoran_m-shiny.gif', 'weezing.gif', 'cubone-shiny.gif', 'omanyte.gif', 'persian-shiny.gif', 'slowpoke-shiny.gif', 'raticate.gif', 'hypno-shiny.gif', 'golbat-shiny.gif', 'jigglypuff.gif', 'poliwag-shiny.gif', 'victreebel.gif', 'vileplume-shiny.gif', 'golem-shiny.gif', 'onix.gif', 'nidorina.gif', 'venonat.gif', 'goldeen-shiny.gif', 'exeggutor-shiny.gif', 'vulpix.gif', 'blastoise-shiny.gif', 'venusaur-shiny.gif', 'tentacruel-shiny.gif', 'doduo.gif', 'dodrio-shiny.gif', 'jigglypuff-shiny.gif', 'tangela-shiny.gif', 'pidgeot-shiny.gif', 'exeggcute.gif', 'clefairy-shiny.gif', 'magneton-shiny.gif', 'articuno-shiny.gif', 'squirtle.gif', 'mankey-shiny.gif', 'omanyte-shiny.gif', 'clefairy.gif', 'ekans-shiny.gif', 'nidoran_m.gif', 'grimer.gif', 'cloyster-shiny.gif', 'koffing-shiny.gif', 'gastly-shiny.gif', 'golduck-shiny.gif', 'slowbro-shiny.gif', 'ninetales.gif', 'sandshrew-shiny.gif', 'dewgong.gif', 'krabby.gif', 'tentacruel.gif', 'eevee-shiny.gif', 'zubat-shiny.gif', 'meowth.gif', 'ninetales-shiny.gif', 'pinsir-shiny.gif', 'bellsprout.gif', 'paras-shiny.gif', 'horsea.gif', 'jynx.gif', 'dewgong-shiny.gif', 'tauros-shiny.gif', 'electrode.gif', 'squirtle-shiny.gif', 'vaporeon-shiny.gif', 'charmander.gif', 'kabuto.gif', 'magmar.gif', 'krabby-shiny.gif', 'electrode-shiny.gif', 'golem.gif', 'horsea-shiny.gif', 'omastar-shiny.gif', 'metapod-shiny.gif', 'clefable.gif', 'fearow-shiny.gif', 'nidorina-shiny.gif', 'charmeleon.gif', 'venomoth.gif', 'omastar.gif', 'psyduck-shiny.gif', 'cloyster.gif', 'diglett-shiny.gif', 'pidgey-shiny.gif', 'shellder.gif', 'charmander-shiny.gif', 'kangaskhan-shiny.gif', 'pidgeotto-shiny.gif', 'starmie-shiny.gif', 'kingler.gif', 'magikarp-shiny.gif', 'ekans.gif', 'mew.gif', 'primeape-shiny.gif', 'slowpoke.gif', 'nidoking-shiny.gif', 'articuno.gif', 'parasect.gif', 'venonat-shiny.gif', 'charizard.gif', 'bulbasaur.gif', 'nidoqueen-shiny.gif', 'rattata-shiny.gif', 'starmie.gif', 'lickitung.gif', 'sandshrew.gif', 'zapdos-shiny.gif', 'alakazam.gif', 'charizard-shiny.gif', 'pikachu-shiny.gif', 'mr-_mime.gif', 'rhydon-shiny.gif', 'ivysaur-shiny.gif', 'nidoking.gif', 'grimer-shiny.gif', 'wartortle-shiny.gif', 'rhyhorn-shiny.gif', 'nidoqueen.gif', 'tentacool-shiny.gif', 'golbat.gif', 'weezing-shiny.gif', 'nidoran_f.gif', 'machop-shiny.gif', 'poliwrath.gif', 'magnemite.gif', 'machoke-shiny.gif', 'dratini.gif', 'voltorb-shiny.gif', 'porygon.gif', 'beedrill.gif', 'butterfree.gif', 'flareon.gif', 'seadra-shiny.gif', 'exeggcute-shiny.gif', 'haunter-shiny.gif', 'machop.gif', 'kabutops-shiny.gif', 'flareon-shiny.gif', 'mr-mime.gif', 'weedle-shiny.gif', 'venusaur.gif', 'seaking.gif', 'muk-shiny.gif', 'cubone.gif', 'machamp-shiny.gif', 'ivysaur.gif', 'wigglytuff.gif', 'seaking-shiny.gif', 'koffing.gif', 'gengar.gif', 'ditto.gif', 'gastly.gif', 'kakuna.gif', 'growlithe.gif', 'rapidash.gif', 'scyther-shiny.gif', 'rhydon.gif', 'caterpie-shiny.gif', 'gengar-shiny.gif', 'tangela.gif', 'nidorino-shiny.gif', 'arbok.gif', 'dragonair.gif', 'pikachu.gif', 'snorlax-shiny.gif', 'rhyhorn.gif', 'machamp.gif', 'geodude-shiny.gif', 'dodrio.gif', 'arbok-shiny.gif', 'tauros.gif', 'alakazam-shiny.gif', 'mewtwo.gif', 'eevee.gif', 'raticate-shiny.gif', 'wartortle.gif', 'aerodactyl-shiny.gif', 'electabuzz.gif', 'gloom.gif', 'pidgey.gif', 'vulpix-shiny.gif', 'shellder-shiny.gif', 'marowak-shiny.gif', 'mankey.gif', 'abra.gif', 'goldeen.gif', 'jynx-shiny.gif', 'lapras.gif', 'gyarados.gif', 'pidgeotto.gif', 'tentacool.gif', 'nidorino.gif', 'seel.gif', 'growlithe-shiny.gif', 'weepinbell.gif', 'beedrill-shiny.gif', 'victreebel-shiny.gif', 'kadabra.gif', 'graveler.gif', 'gyarados-shiny.gif', 'primeape.gif', 'doduo-shiny.gif', 'dragonite-shiny.gif', 'spearow-shiny.gif', 'oddish-shiny.gif', 'pinsir.gif', 'lapras-shiny.gif', 'gloom-shiny.gif', 'kakuna-shiny.gif', 'geodude.gif', 'electabuzz-shiny.gif', 'scyther.gif', 'meowth-shiny.gif', 'mew-shiny.gif', 'zubat.gif', 'blastoise.gif', 'vileplume.gif', 'voltorb.gif', 'sandslash-shiny.gif', 'snorlax.gif', 'persian.gif', 'weepinbell-shiny.gif', 'rapidash-shiny.gif', 'rattata.gif', 'magikarp.gif', 'parasect-shiny.gif', 'onix-shiny.gif', 'drowzee.gif', 'hypno.gif', 'weedle.gif', 'farfetchd-shiny.gif', 'metapod.gif', 'charmeleon-shiny.gif', 'dragonite.gif', 'moltres.gif', 'jolteon.gif', 'lickitung-shiny.gif', 'bulbasaur-shiny.gif', 'exeggutor.gif', 'marowak.gif', 'magneton.gif']
 
+let showText = false;
+
 const shinyImages = images.filter(img => img.includes("-shiny"));
 const regImages = images.filter(img => !img.includes("-shiny"));
 
+const pokemonImage = document.querySelector("#pokemon");
+
+pokemonImage.src = getRandomPokemon();
+
 function getRandomPokemon() {
-  const shinyChance = 1 / 50;
+  const shinyChance = 1 / 100;
   const isShiny = Math.random() < shinyChance;
 
   if (isShiny) {
+    show();
     return "sprites/" + shinyImages[Math.floor(Math.random() * shinyImages.length)];
   }
 
   return "sprites/" + regImages[Math.floor(Math.random() * regImages.length)];
-
 }
 
-document.querySelector("#image").src = getRandomPokemon();
+function show() {
+  showText = true;
+  render();
+}
+
+function render() {
+  const p = document.getElementById("shiny");
+
+  if (showText) {
+    p.innerText = "shiny!"
+  }
+  else {
+    p.innerText = "";
+  }
+}
